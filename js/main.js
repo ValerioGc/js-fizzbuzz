5a309361d89fec3fef46b6bf6e87aff749f9087a
ValerioGc <valerio.gc1@gmail.com>
2022-07-06 17:11:51 +0200

    Revisione

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 const submit = document.getElementById('submit');
+const refresh = document.getElementById('refresh');
+refresh.addEventListener('click',
+    function() {
+        location.reload()
+    });
 submit.addEventListener('click', 
     function() {
-        document.getElementById('page').className = "show";
         const container = document.getElementById('page');
         const user_input = parseInt(document.getElementById('number_cell').value); 
-        console.log(` Il tipo di dato inserito è (typeof ${user_input}) Il numero scelto è ${user_input}`);
-        console.log(user_input);
-        for (let i = 0; i <= user_input; i++) {
-            const containerInner = document.createElement('div');
-            containerInner.append(i);
-            containerInner.className = 'box';
-            container.append(containerInner);
+        if ((isNaN(user_input)) || (user_input == '')){
+            alert('Attenzione! Dati inseriti non corretti')
+            location.reload();
+        }
+        else {
+            document.getElementById('page').className = "show";
+            console.log(` Il tipo di dato inserito è ` + typeof(user_input) + ' ' + `Il numero scelto è ${user_input}`);
+            console.log(user_input);
+            for (let i = 1; i <= user_input; i++) {
+                const containerInner = document.createElement('div');
+                containerInner.append(i);
+                if ((i % 3 == 0) && (i % 5 == 0)) {
+                    containerInner.className = 'box fizzbuzz';
+                    containerInner.innerHTML = 'FizzBuzz';
+                }
+                else if (i % 3 == 0) {
+                    containerInner.className = 'box fizz';
+                    containerInner.innerHTML = 'Fizz';
+                }
+                else if (i % 5 == 0) {
+                    containerInner.className = 'box buzz';
+                    containerInner.innerHTML = 'Buzz';
+                }
+                else {
+                    containerInner.className = 'box';
+                }
+                container.append(containerInner);
+            }
         }
 });
+
